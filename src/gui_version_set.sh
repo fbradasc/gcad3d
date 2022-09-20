95f2986c1592d211d49e8c280a5c108a07caaf7b
Francesco Bradascio <francesco.bradascio@consulenti.urmet.com>
2022-09-20 13:34:18 +0200

    Use pkg-config to get the GTK installed version before checking for installation type's packages managers

--- src/gui_version_set.sh
+++ src/gui_version_set.sh
@@ -15,16 +15,17 @@ echo "instTyp = |"${instTyp}"|"
 
 
 if [ $1 = "gtk2" ] ; then
-  if [ ${instTyp} = "deb" ] ; then
-    if [ -z "`dpkg -l | grep libgtk2.0-dev`" ] ; then
-       echo "**** gui_version_set.sh ERROR gtk2 not installed"
-       exit 1
-    fi
-  else
-#   if [ -z "`pkg-config --list-all | grep gtk+-2.0`" ] ; then
-    if [ -z "`rpm -qa | grep gtk2-dev`" ] ; then
-       echo "**** gui_version_set.sh ERROR gtk2 not installed"
-       exit 1
+  if [ -z "`pkg-config --list-all | grep gtk+-2.0`" ] ; then
+    if [ ${instTyp} = "deb" ] ; then
+      if [ -z "`dpkg -l | grep libgtk2.0-dev`" ] ; then
+         echo "**** gui_version_set.sh ERROR gtk2 not installed"
+         exit 1
+      fi
+    else
+      if [ -z "`rpm -qa | grep gtk2-dev`" ] ; then
+         echo "**** gui_version_set.sh ERROR gtk2 not installed"
+         exit 1
+      fi
     fi
   fi
   echo "gtk2" > ../gcad_gui_version
@@ -33,16 +34,17 @@ fi
 
 
 if [ $1 = "gtk3" ] ; then
-  if [ ${instTyp} = "deb" ] ; then
-    if [ -z "`dpkg -l | grep libgtk-3-dev`" ] ; then
-       echo "**** gui_version_set.sh ERROR gtk3 not installed"
-       exit 1
-    fi
-  else
-#   if [ -z "`pkg-config --list-all | grep gtk+-3.0`" ] ; then
-    if [ -z "`rpm -qa | grep gtk3-dev`" ] ; then
-       echo "**** gui_version_set.sh ERROR gtk3 not installed"
-       exit 1
+  if [ -z "`pkg-config --list-all | grep gtk+-3.0`" ] ; then
+    if [ ${instTyp} = "deb" ] ; then
+      if [ -z "`dpkg -l | grep libgtk-3-dev`" ] ; then
+         echo "**** gui_version_set.sh ERROR gtk3 not installed"
+         exit 1
+      fi
+    else
+      if [ -z "`rpm -qa | grep gtk3-dev`" ] ; then
+         echo "**** gui_version_set.sh ERROR gtk3 not installed"
+         exit 1
+      fi
     fi
   fi
   echo "gtk3" > ../gcad_gui_version
